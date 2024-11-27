191fe47acb8900fdb7476fa24a6dbc7423dfba58
Islam <9337@holbertonstudents.com>
2024-11-27 15:04:59 +0400

    Fix 0-get_list_students.js

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,10 @@
+//getListStudents()
+
 const getListStudents = () => {
         const students = [
                         {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
                         {id: 2, firstName: 'James', location: 'Columbia'},
-                        {id: 3, firstName: 'Serena', location: 'San Francisco'}
+                        {id: 3, firstName: 'Serena', location: 'San Francisco'},
                 ];
         return students;
 }
