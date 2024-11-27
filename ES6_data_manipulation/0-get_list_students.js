179f9b4c147ee78ce60da2345c48b8c9e8276baa
Islam <9337@holbertonstudents.com>
2024-11-27 21:44:41 +0400

    Fix eslint error

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,15 +1,3 @@
-//getListStudents()
-
-// const getListStudents = () => {
-//         const students = [
-//                         {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-//                         {id: 2, firstName: 'James', location: 'Columbia'},
-//                         {id: 3, firstName: 'Serena', location: 'San Francisco'},
-//                 ];
-//         return students;
-// }
-
-// export default getListStudents;
 const getListStudents = () => {
         const students = [
           { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
