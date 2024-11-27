2a44d59afa066d6a53db64d9f3175053df723f8e
Islam <9337@holbertonstudents.com>
2024-11-27 15:08:10 +0400

    Test

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,12 +1,23 @@
 //getListStudents()
 
+// const getListStudents = () => {
+//         const students = [
+//                         {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
+//                         {id: 2, firstName: 'James', location: 'Columbia'},
+//                         {id: 3, firstName: 'Serena', location: 'San Francisco'},
+//                 ];
+//         return students;
+// }
+
+// export default getListStudents;
 const getListStudents = () => {
         const students = [
-                        {id: 1, firstName: 'Guillaume', location: 'San Francisco'},
-                        {id: 2, firstName: 'James', location: 'Columbia'},
-                        {id: 3, firstName: 'Serena', location: 'San Francisco'},
-                ];
+          { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+          { id: 2, firstName: 'James', location: 'Columbia' },
+          { id: 5, firstName: 'Serena', location: 'San Francisco' },
+        ];
+      
         return students;
-}
-
-export default getListStudents;
+      };
+      
+      export default getListStudents;
