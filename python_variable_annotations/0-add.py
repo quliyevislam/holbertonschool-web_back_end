2b4625297169eb934b2f186855e1dbc0fb9e76d7
Islam <9337@holbertonstudents.com>
2024-12-08 12:48:36 +0400

    Add 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 import typing
+"""This module contains add() with type annotation"""
 
 
 def add(a: float, b: float) -> float:
+    """
+    Adds two floating-point numbers.
+
+    :param a: The first number (float).
+    :param b: The second number (float).
+    :return: The sum of `a` and `b` (float).
+    """
     return a + b
