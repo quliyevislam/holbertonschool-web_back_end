c362683d8858d4ea53a83802e487768fc10e8b52
Islam <9337@holbertonstudents.com>
2024-12-15 18:12:19 +0400

    Fix pycode style and doc checker

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python3
+"""This module contains async_generator() function"""
 import asyncio
 import random
 
+
 async def async_generator():
+    """
+    Generates random values between 0 and 10
+    """
     for _ in range(10):
         await asyncio.sleep(1)
-        yield random.uniform(0,10)
+        yield random.uniform(0, 10)
