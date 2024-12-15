3a483b7b3857e03bb13cbcba2d02b6708b8a1ce2
Islam <9337@holbertonstudents.com>
2024-12-15 18:33:22 +0400

    Add 1-async_comprehension.py

---
+++ python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""This module contains async_comprehension() function"""
+import asyncio
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """
+       Asynchronously collects values from an async generator and returns
+       them as a list.
+
+       It iterates over values produced by the `async_generator()`
+       and appends them to a list.
+
+       Returns:
+           List[float]: A list of float values from the async generator.
+       """
+    nums = []
+    async for num in async_generator():
+        nums.append(num)
+    return nums
