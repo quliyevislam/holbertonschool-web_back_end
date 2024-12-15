864f91dd562c028cc91a828d3624e4303194c495
Islam <9337@holbertonstudents.com>
2024-12-15 18:18:16 +0400

    Fix

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,9 +4,15 @@ import asyncio
 import random
 from typing import Generator
 
+
 async def async_generator() -> Generator[float, None, None]:
     """
-    Generates random values between 0 and 10
+           Generate numbers
+           Args:
+               void
+
+           Return:
+                float numbers
     """
     for _ in range(10):
         await asyncio.sleep(1)
