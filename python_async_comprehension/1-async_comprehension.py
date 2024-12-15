f3cf3e0c37c996b3f75f5c6d1a7230b84a1f1418
Islam <9337@holbertonstudents.com>
2024-12-15 18:36:06 +0400

    fix

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,16 +7,14 @@ async_generator = __import__('0-async_generator').async_generator
 
 async def async_comprehension() -> List[float]:
     """
-       Asynchronously collects values from an async generator and returns
-       them as a list.
+            Generate numbers with async comprenhension
 
-       It iterates over values produced by the `async_generator()`
-       and appends them to a list.
-        args:
-            void
-       Returns:
-           List[float]: A list of float values from the async generator.
-       """
+            Args:
+                void
+
+            Return:
+                float random numbers
+        """
     nums = []
     async for num in async_generator():
         nums.append(num)
