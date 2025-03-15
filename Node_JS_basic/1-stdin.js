a0a498063a2b22ed37cc333bffa6185ad4386634
quliyevislam <9337@holbertonstudents.com>
2025-03-15 20:11:15 +0400

    Add

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,10 @@
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
 process.stdin.on('readable', () => {
-  const name = process.stdin.read();
-
-  if ((name) !== null) {
-    process.stdout.write(`Your name is: ${name}`);
+  const chunk = process.stdin.read();
+  if (chunk !== null) {
+    process.stdout.write(`Your name is: ${chunk}`);
+    console.log('This important software is now closing');
   }
-});
-process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
 });
