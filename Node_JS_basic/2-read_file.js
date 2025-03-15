6f5431b017856fe22153192f7d1ceac82ef97f2a
quliyevislam <9337@holbertonstudents.com>
2025-03-15 20:12:12 +0400

    Add

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,46 +1,24 @@
-const fs = require('fs');
+const Fs = require('fs');
 
-function countStudents(filepath) {
+function countStudents(path) {
   try {
-      const csv = fs.readFileSync(filepath, { encoding: 'utf8' });
-      const headerArray = csv.split(/\r?\n|\n/);
-      const headers = headerArray[0].split(',');
-
-      const dictList = [];
-      const noHeaderArray = headerArray.slice(1);
-      for (let i = 0; i < noHeaderArray.length; i += 1) {
-          const data = noHeaderArray[i].split(',');
-          if (data.length === headers.length) {
-              const row = {};
-              for (let j = 0; j < headers.length; j += 1) {
-                  row[headers[j].trim()] = data[j].trim();
-              }
-              dictList.push(row);
-          }
-      }
-
-      let countCS = 0;
-      let countSWE = 0;
-      const studentsCS = [];
-      const studentsSWE = [];
-
-      dictList.forEach((element) => {
-          if (element.field === 'CS') {
-              countCS += 1;
-              studentsCS.push(element.firstname);
-          } else if (element.field === 'SWE') {
-              countSWE += 1;
-              studentsSWE.push(element.firstname);
-          }
-      });
-
-      const countStudents = countCS + countSWE;
-
-      console.log(`Number of students: ${countStudents}`);
-      console.log(`Number of students in CS: ${countCS}. List: ${studentsCS.toString().split(',').join(', ')}`);
-      console.log(`Number of students in SWE: ${countSWE}. List: ${studentsSWE.toString().split(',').join(', ')}`);
+    let databaseFile = Fs.readFileSync(path, 'utf8').toString();
+    databaseFile = databaseFile.split('\n');
+    databaseFile.shift();
+    const numOfStudents = databaseFile.length;
+    const result = [`Number of students: ${numOfStudents}`, [], []];
+    databaseFile.forEach((element) => {
+      const resultElement = element.split(',');
+      if (resultElement[3] === 'CS') {
+        result[1].push(resultElement[0]);
+      } else if (resultElement[3] === 'SWE') { result[2].push(resultElement[0]); }
+    });
+    const finalResult = `${result[0]}\nNumber of students in CS: ${result[1].length}. List: ${result[1].toString().split(',').join(', ')}\nNumber of students in SWE: ${result[2].length}. List: ${result[2].toString().split(',').join(', ')}`;
+    console.log(finalResult);
   } catch (err) {
+    if (err.code === 'ENOENT') {
       throw new Error('Cannot load the database');
+    }
   }
 }
 
